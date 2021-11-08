ea5b203b80bee6bef4bdaa92a783e46ba8576ae0
dev-swd <tladnjsen@gmail.com>
2021-11-08 23:52:25 +0900

    <feature:8.1>: test fuction component using youtube data api - test useState, useEffect

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,25 @@
 /** @format */
-
+import React, { useEffect, useState } from 'react';
 import './app.css';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch(
+      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=AIzaSyClLfVPEfqdpMRJ55ggbEuUXp6tMToLMsw',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log('error', error));
+  }, []);
+
   return <h1>Ready?</h1>;
 }
 
