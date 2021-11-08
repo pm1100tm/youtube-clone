e14c949d617842c0e167d0c17fc5f937e2578363
dev-swd <tladnjsen@gmail.com>
2021-11-09 00:37:41 +0900

    <feature:8.1>: show video list and title - show video item title in video list

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,27 @@
 /** @format */
-
+import React, { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return <h1>Ready?</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch(
+      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=KR&key=',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log('error', error));
+  }, []);
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
