fc665e8ae35fbf70d2978081df3b636e8c9a8840
dev-swd <tladnjsen@gmail.com>
2021-11-15 00:24:58 +0900

    <feature:8.6>: Add Header - add favicon, logo, search image - add header for search ui

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 /** @format */
 import React, { useEffect, useState } from 'react';
-import './app.css';
+import styles from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
 function App() {
@@ -21,7 +22,14 @@ function App() {
       .catch((error) => console.log('error', error));
   }, []);
 
-  return <VideoList videos={videos} />;
+  return (
+    <>
+      <div className={styles.app}>
+        <SearchHeader />
+        <VideoList videos={videos} />
+      </div>
+    </>
+  );
 }
 
 export default App;
