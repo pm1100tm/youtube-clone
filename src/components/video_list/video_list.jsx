7e3f56dd526efac74884906a1e48e24baa9f94a3
dev-swd <tladnjsen@gmail.com>
2021-11-14 18:24:47 +0900

    <feature:8.4>: Add post css to video list, item - add css module to video_list, video_item component

--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -2,9 +2,10 @@
 
 import React from 'react';
 import VideoItem from '../video_item/video_item';
+import style from './video_list.module.css';
 
 const VideoList = (props) => (
-  <ul>
+  <ul className={style.videoList}>
     {props.videos.map((video) => (
       <VideoItem key={video.id} video={video} />
     ))}
