1730529bbdd3536a4fbb423e045df9c184c65b8a
dev-swd <tladnjsen@gmail.com>
2021-12-12 18:23:02 +0900

    <feature:8.8>: refactoring component method - add service forder to define network method - define a instance of YoutubeNetwork class at index.js - call the youtubeNetwork method at component as  MVC pattern

--- src/app.jsx
+++ src/app.jsx
@@ -4,39 +4,18 @@ import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
-function App() {
+function App({youtubeNetwork}) {
   const [videos, setVideos] = useState([]);
 
-  const search = (inputSearchValue) => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video&q=${inputSearchValue}&key=`,
-      requestOptions
-    )
-    .then((response) => response.json())
-    .then((result) => result.items.map((item) => ({ ...item, id: item.id.videoId })))
-    .then((items) => setVideos(items))
-    .catch((error) => console.log('error', error));
-  };
-
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-
-    fetch(
-      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=KR&maxResults=25&key=',
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log('error', error));
+    youtubeNetwork.mostPopular()
+    .then((videos) => setVideos(videos))
   }, []);
+
+  const search = (inputSearchValue) => {
+    youtubeNetwork.searchByInputValue(inputSearchValue)
+    .then((videos) => setVideos(videos))
+  };
 
   return (
     <>
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import YoutubeNetwork from './service/youtube';
 
+const youtubeNetwork = new YoutubeNetwork('test');
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App youtubeNetwork={youtubeNetwork} />
   </React.StrictMode>,
   document.getElementById('root')
 );
