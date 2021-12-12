e60452a368057928f78c4ae7ef611f5bb28f05a9
dev-swd <tladnjsen@gmail.com>
2021-12-12 18:23:02 +0900

    <feature:8.8>: refactoring component method - add service forder to define network method - define a instance of YoutubeNetwork class at index.js - call the youtubeNetwork method at component as  MVC pattern

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -3,7 +3,7 @@
 class YoutubeNetwork {
   constructor(key) {
     this.key = key;
-    this.requestOptions = {
+    this.getRequestOptions = {
       method: 'GET',
       redirect: 'follow'
     };
@@ -11,8 +11,8 @@ class YoutubeNetwork {
 
   mostPopular() {
     return fetch(
-      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=KR&maxResults=25&key=',
-      this.requestOptions
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=KR&maxResults=25&key=AIzaSyDi50DRnmdHRQU_vfaocaFxkJgMk8J_Avs`,
+      this.getRequestOptions
     )
       .then((response) => response.json())
       .then((result) => result.items);
@@ -20,10 +20,12 @@ class YoutubeNetwork {
 
   searchByInputValue(inputSearchValue) {
     return fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video&q=${inputSearchValue}&key=`,
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video&q=${inputSearchValue}&key=AIzaSyDi50DRnmdHRQU_vfaocaFxkJgMk8J_Avs`,
       this.requestOptions
     )
       .then((response) => response.json())
       .then((result) => result.items.map((item) => ({ ...item, id: item.id.videoId })));
   }
 }
+
+export default YoutubeNetwork;
