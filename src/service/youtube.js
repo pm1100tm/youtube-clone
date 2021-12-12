5d2eb9148d41127052bdac01006ab407030bbbaa
dev-swd <tladnjsen@gmail.com>
2021-12-12 17:15:14 +0900

    Add src/service/youtube.js

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -3,28 +3,28 @@
 class YoutubeNetwork {
   constructor(key) {
     this.key = key;
-    this.getRequestOptions = {
+    this.requestOptions = {
       method: 'GET',
       redirect: 'follow'
     };
   }
 
-  mostPopular() {
-    return fetch(
+  async mostPopular() {
+    const response = await fetch(
       `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=KR&maxResults=25&key=${this.key}`,
       this.getRequestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => result.items);
+    );
+    const result = await response.json();
+    return result.items;
   }
 
-  searchByInputValue(inputSearchValue) {
-    return fetch(
+  async searchByInputValue(inputSearchValue) {
+    const response = await fetch(
       `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video&q=${inputSearchValue}&key=${this.key}`,
       this.requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => result.items.map((item) => ({ ...item, id: item.id.videoId })));
+    );
+    const result = await response.json();
+    return result.items.map((item) => ({ ...item, id: item.id.videoId }));
   }
 }
 
