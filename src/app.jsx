fbcc84631468dbdd0818830f7c1b0fcd282a7b4a
dev-swd <tladnjsen@gmail.com>
2021-12-19 12:24:47 +0900

    <feature:8.12>: modify search event - set selected video status null, when calling search

--- src/app.jsx
+++ src/app.jsx
@@ -3,38 +3,43 @@ import React, { useEffect, useState } from 'react';
 import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
-import VideoDetail from './components/video_detail/video_detail'
+import VideoDetail from './components/video_detail/video_detail';
 
-function App({youtubeNetwork}) {
+function App({ youtubeNetwork }) {
   const [videos, setVideos] = useState([]);
-  const [selectedVideo, setSelectedVideo] = useState(null)
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
   useEffect(() => {
-    youtubeNetwork.mostPopular()
-    .then((videos) => setVideos(videos))
+    youtubeNetwork.mostPopular().then((videos) => setVideos(videos));
   }, []);
 
   const search = (inputSearchValue) => {
-    youtubeNetwork.searchByInputValue(inputSearchValue)
-    .then((videos) => setVideos(videos))
+    setSelectedVideo(null);
+    youtubeNetwork.searchByInputValue(inputSearchValue).then((videos) => {
+      setVideos(videos);
+    });
   };
 
   const selectVideo = (video) => {
-    setSelectedVideo(video)
-  }
+    setSelectedVideo(video);
+  };
 
   return (
     <>
       <div className={styles.app}>
         <SearchHeader onSearch={search} />
         <section className={styles.content}>
-          { selectedVideo &&
+          {selectedVideo && (
             <div className={styles.detail}>
               <VideoDetail video={selectedVideo} />
             </div>
-          }
+          )}
           <div className={styles.list}>
-            <VideoList videos={videos} onVideoClick={selectVideo} display={selectedVideo ? 'list' : 'grid'} />
+            <VideoList
+              videos={videos}
+              onVideoClick={selectVideo}
+              display={selectedVideo ? 'list' : 'grid'}
+            />
           </div>
         </section>
       </div>
