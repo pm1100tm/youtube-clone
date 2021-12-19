1bdc9354f1517f5efa16f1fa972227830c55565c
dev-swd <tladnjsen@gmail.com>
2021-12-19 13:55:12 +0900

    <feature:8.13>: use memo - use memo to video item and search header

--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -6,14 +6,15 @@ import styles from './video_detail.module.css';
 const VideoDetail = ({ video, video: { snippet } }) => (
   <section className={styles.detail}>
     <iframe
+      title="youtube video player"
       className={styles.videoBox}
       id="ytplayer"
       type="text/html"
       width="100%"
       height="500px"
       src={`https://www.youtube.com/embed/${video.id}`}
-      frameborder="0"
-      allowfullscreen
+      frameBorder="0"
+      allow="fullscreen"
     ></iframe>
     <h1>{snippet.title}</h1>
     <h1>{snippet.channelTitle}</h1>
